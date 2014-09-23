085290077f578fc45b0832eebf74af02d962ff00
Akul Penugonda <apenugonda@Akuls-MacBook-Air.local>
2014-09-22 21:59:10 -0400

    Clarify projects

---
+++ FeatureDetector/main.cpp
@@ -0,0 +1,75 @@
+//
+//  main.cpp
+//  FeatureDetector
+//
+//  Created by Akul Penugonda on 9/22/14.
+//  Copyright (c) 2014 Akul Penugonda. All rights reserved.
+//
+
+#include "DetectionPipeline.h"
+#include <stdio.h>
+#include <iostream>
+#include <dirent.h>
+
+#define TRAINING_DATA "/Users/apenugonda/Documents/fridges-are-cool/fern/images/train"
+
+using namespace cv;
+using namespace std;
+
+bool has_suffix(const std::string &str, const std::string &suffix)
+{
+    return str.size() >= suffix.size() &&
+    str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
+}
+
+char* subdirString(char* parent, char* name) {
+    char* new_str = (char*)malloc(strlen(name) + strlen(parent) + 2);
+    new_str[0] = '\0';
+    strcat(new_str, parent);
+    strcat(new_str, "/");
+    strcat(new_str, name);
+    return new_str;
+}
+
+int main(int argc, const char * argv[])
+{
+    //Iterate through subdirectories
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(TRAINING_DATA);
+    DetectionPipeline pipeline;
+    if (d) {
+        //Iterate through each directory contained here
+        char* subdirName;
+        char* imageName;
+        while ((dir = readdir(d)) != NULL) {
+            //Don't count current/parent dir/DS_STORE
+            if (!strcmp(dir->d_name, ".") || !strcmp(dir->d_name, "..") || !strcmp(dir->d_name, ".DS_Store")) {
+                continue;
+            }
+            printf("Loading Object %s\n", dir->d_name);
+            subdirName = subdirString(TRAINING_DATA, dir->d_name);
+            DIR *d2;
+            struct dirent *dir2;
+            d2 = opendir(subdirName);
+            while ((dir2 = readdir(d2)) != NULL) {
+                
+                //Can only be a JPG file
+                if (!has_suffix(dir2->d_name, ".jpg")) {
+                    continue;
+                }
+                
+                imageName = subdirString(subdirName, dir2->d_name);
+                printf("Image name: %s\n", imageName);
+                pipeline.setObjType(dir->d_name);
+                pipeline.setImageName(dir2->d_name);
+                pipeline.runPipeline(imageName);
+                free(imageName);
+            }
+            free(subdirName);
+        }
+    }
+    waitKey(0);
+    return 0;
+}
+
